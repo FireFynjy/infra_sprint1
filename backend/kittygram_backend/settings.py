39187038532666ad6e5203f2c7ab77600efce465
FireFynjy <firefynjy@gmail.com>
2023-07-07 07:10:53 +0000

    Review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['84.252.136.83', '127.0.0.1', 'localhost', 'kittygramtest.hopto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
