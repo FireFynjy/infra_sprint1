64c9d5bdd26096bcd7c00da88fd65713181d29e4
FireFynjy <82879373+FireFynjy@users.noreply.github.com>
2023-07-08 15:00:22 +0300

    Review

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = bool(int(os.getenv('DEBUG')))
+DEBUG = bool(int(os.getenv('DEBUG', '0')))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
